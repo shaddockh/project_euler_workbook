594c03222403948498cb42db22cef1fa2bb2e447
Shaddock Heath <shaddockh@gmail.com>
2013-08-30 12:42:47 -0400

    added more helpers to assist with unit testing

--- src/utils/test.js
+++ src/utils/test.js
@@ -3,3 +3,13 @@ exports.assertEqual = function(expected, actual, msg) {
         throw new Error('Assert Error. Expected: ' + expected + ' Got: ' + actual + '\n' + (msg || ''));
     }
 };
+
+exports.run = function(name, test) {
+    try {
+        test();
+        console.log(name + ': Passed');
+    } catch (e) {
+        console.log(name + ': Failed - ' + e.message);
+    }
+};
+
