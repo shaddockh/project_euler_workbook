7e0b925a83ced844d18a95a5e303f752d3ec9057
Shaddock Heath <shaddockh@gmail.com>
2013-09-06 12:03:13 -0400

    added ability to disable tests

--- src/utils/test.js
+++ src/utils/test.js
@@ -5,11 +5,19 @@ exports.assertEqual = function(expected, actual, msg) {
 };
 
 exports.run = function(name, test) {
-    try {
-        test();
-        console.log(name + ': Passed');
-    } catch (e) {
-        console.log(name + ': Failed - ' + e.message);
+    if (exports.enabled) {
+
+        var red = '\u001b[31m';
+        var green = '\u001b[32m';
+        var reset = '\u001b[0m';
+        var blue = '\u001b[34m';
+        try {
+            test();
+            console.log(blue + name + ': ' + green + 'Passed' + reset);
+        } catch (e) {
+            console.log(blue + name + ': ' + red + 'Failed' + reset + ' - ' + e.message);
+        }
     }
 };
 
+exports.enabled = true;
